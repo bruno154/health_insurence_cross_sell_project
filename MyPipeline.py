c3b8ddfe9f5789a9888c454f42addcd80fed5274
bruno154 <brunovinicius154@gmail.com>
2021-03-06 22:34:58 -0300

    working on models

---
+++ MyPipeline.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class preProcessing(BaseEstimator, TransfomerMixin):
+    
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y=None):
+        return self
+        
+    def transform(self, X, y=None):
+        
+        Xtemp = X.copy()
+        
+        
+        
+        
+        
+        
+        
+        
+        return Xtemp
